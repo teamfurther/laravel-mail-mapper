3cebfaa649c4c5b6cacb5401e5347f8e6388153d
Csongor Ur <csongor.ur@c4studio.ro>
2020-05-08 18:18:51 +0300

    Command added.

---
+++ src/Console/Commands/GoogleOAuthCommand.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Further\Mailmatch\Console\GoogleOauthCommand;
+
+use Illuminate\Console\Command;
+
+class GoogleOAuthCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mailmatch:generate-google-oauth-token';
+
+    /**
+     * @var
+     */
+    protected $googleClient;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generates the OAuth token to used by the Gmail API.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+    }
+
+    private function getClient()
+    {
+        $client = new Google_Client();
+        $client->setApplicationName('Gmail API PHP Quickstart');
+        $client->setScopes(Google_Service_Gmail::GMAIL_READONLY);
+        $client->setAuthConfig('credentials.json');
+        $client->setAccessType('offline');
+        $client->setPrompt('select_account consent');
+        //$client->setRedirectUri("https://laravelmailmatch.test");
+
+        // Load previously authorized token from a file, if it exists.
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        $tokenPath = 'token.json';
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                // Request authorization from the user.
+                $authUrl = $client->createAuthUrl();
+                printf("Open the following link in your browser:\n%s\n", $authUrl);
+                print 'Enter verification code: ';
+                $authCode = trim(fgets(STDIN));
+
+                // Exchange authorization code for an access token.
+                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                $client->setAccessToken($accessToken);
+
+                // Check to see if there was an error.
+                if (array_key_exists('error', $accessToken)) {
+                    throw new Exception(join(', ', $accessToken));
+                }
+            }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0700, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
+        return $client;
+    }
+}
